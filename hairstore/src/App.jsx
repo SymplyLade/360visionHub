3390a4fc05d4aa9ffa6deebd0f8152fc57fee512
Gbemisola victoria <vgbemisola3@gmail.com>
2026-01-14 12:55:25 +0100

    hairstore1

--- hairstore/src/App.jsx
+++ hairstore/src/App.jsx
@@ -1,8 +1,127 @@
+// import { useState, useEffect } from "react";
+// import localforage from "localforage";
+// import ProductList from "./components/ProductList";
+// import AddProduct from "./components/AddProduct";
+// import "./App.css";
+
+// export default function App() {
+//   const [products, setProducts] = useState([]);
+//   const [isAdmin, setIsAdmin] = useState(false);
+//   const [showLoginModal, setShowLoginModal] = useState(false);
+//   const [passwordInput, setPasswordInput] = useState("");
+
+//   const ADMIN_PASSWORD = "Jayden"; 
+
+ 
+//   useEffect(() => {
+//     async function loadProducts() {
+//       const savedProducts = await localforage.getItem("products");
+//       if (savedProducts) setProducts(savedProducts);
+//     }
+//     loadProducts();
+//   }, []);
+
+  
+//   useEffect(() => {
+//     localforage.setItem("products", products);
+//   }, [products]);
+
+ 
+//   function addProduct(product) {
+//     setProducts([...products, { ...product, id: Date.now() }]);
+//   }
+
+
+//   function deleteProduct(id) {
+//     setProducts(products.filter((p) => p.id !== id));
+//   }
+
+
+//   function handleLogin() {
+//     if (passwordInput === ADMIN_PASSWORD) {
+//       setIsAdmin(true);
+//       localStorage.setItem("isAdmin", "true");
+//       setShowLoginModal(false);
+//       setPasswordInput("");
+//     } else {
+//       alert("Incorrect password");
+//     }
+//   }
+
+//   function handleLogout() {
+//     setIsAdmin(false);
+//     localStorage.removeItem("isAdmin");
+//     alert("Logged out. You are now in client mode.");
+//   }
+
+//   useEffect(() => {
+//     const savedAdmin = localStorage.getItem("isAdmin");
+//     if (savedAdmin === "true") setIsAdmin(true);
+//   }, []);
+
+//   return (
+//     <div className="app-container">
+//       <h1>360visionHub</h1>
+
+//       <div className="admin-buttons">
+//         {!isAdmin ? (
+//           <button
+//             className="admin-login"
+//             onClick={() => setShowLoginModal(true)}
+//           >
+//             Admin Login
+//           </button>
+//         ) : (
+//           <button className="admin-logout" onClick={handleLogout}>
+//             Admin Logout
+//           </button>
+//         )}
+//       </div>
+
+   
+//       {showLoginModal && (
+//         <div className="modal-overlay">
+//           <div className="modal">
+//             <h3>Admin Login</h3>
+//             <input
+//               type="password"
+//               placeholder="Enter password"
+//               value={passwordInput}
+//               onChange={(e) => setPasswordInput(e.target.value)}
+//             />
+//             <div className="modal-buttons">
+//               <button onClick={handleLogin}>Login</button>
+//               <button
+//                 onClick={() => {
+//                   setShowLoginModal(false);
+//                   setPasswordInput("");
+//                 }}
+//               >
+//                 Cancel
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+
+
+//       {isAdmin && <AddProduct addProduct={addProduct} />}
+
+//       <ProductList
+//         products={products}
+//         readOnly={!isAdmin}
+//         deleteProduct={deleteProduct}
+//       />
+//     </div>
+//   );
+// }
+
+
 import { useState, useEffect } from "react";
-import localforage from "localforage";
 import ProductList from "./components/ProductList";
 import AddProduct from "./components/AddProduct";
 import "./App.css";
+import hardcodedProducts from "./products"; 
 
 export default function App() {
   const [products, setProducts] = useState([]);
@@ -12,26 +131,16 @@ export default function App() {
 
   const ADMIN_PASSWORD = "Jayden"; 
 
- 
   useEffect(() => {
-    async function loadProducts() {
-      const savedProducts = await localforage.getItem("products");
-      if (savedProducts) setProducts(savedProducts);
-    }
-    loadProducts();
+    setProducts(hardcodedProducts);
   }, []);
 
   
-  useEffect(() => {
-    localforage.setItem("products", products);
-  }, [products]);
-
- 
   function addProduct(product) {
     setProducts([...products, { ...product, id: Date.now() }]);
   }
 
-
+ 
   function deleteProduct(id) {
     setProducts(products.filter((p) => p.id !== id));
   }
@@ -43,16 +152,19 @@ export default function App() {
       localStorage.setItem("isAdmin", "true");
       setShowLoginModal(false);
       setPasswordInput("");
+      alert("Admin mode activated!");
     } else {
       alert("Incorrect password");
     }
   }
+
 
   function handleLogout() {
     setIsAdmin(false);
     localStorage.removeItem("isAdmin");
     alert("Logged out. You are now in client mode.");
   }
+
 
   useEffect(() => {
     const savedAdmin = localStorage.getItem("isAdmin");
@@ -63,6 +175,7 @@ export default function App() {
     <div className="app-container">
       <h1>360visionHub</h1>
 
+    
       <div className="admin-buttons">
         {!isAdmin ? (
           <button
@@ -104,7 +217,7 @@ export default function App() {
         </div>
       )}
 
-
+     
       {isAdmin && <AddProduct addProduct={addProduct} />}
 
       <ProductList
---
+++ hairstore/src/products.js
@@ -0,0 +1,59 @@
+// src/products.js
+const products = [
+  {
+    id: 1,
+    name: "Short Curly Pixie Cut Lace Front Wig",
+    price: 15000,
+    image: "https://m.media-amazon.com/images/I/61ib++ALP8L._AC_UY1000_.jpg"
+  },
+  {
+    id: 2,
+    name: "Full Lace",
+    price: 96000,
+    image: "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/71/1797814/1.jpg?0960"
+  },
+  {
+    id: 3,
+    name: "Bob Wig",
+    price: 50000,
+    image: "https://img-1.kwcdn.com/product/fancy/26c323db-c3ec-48d7-b506-0169d5917894.jpg?imageView2/2/w/800/q/70/format/avif"
+  },
+   {
+    id: 3,
+    name: "Wet and Wavy kinky curl hair",
+    price: 70000,
+    image: "https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/90/8782704/1.jpg?1831"
+  },
+   {
+    id: 3,
+    name: "Afro Hair",
+    price: 19500,
+    image: "https://img-1.kwcdn.com/product/Fancyalgo/VirtualModelMatting/3183f93c430ac96104aa925422cbf833.jpg?imageView2/2/w/800/q/70/format/avif"
+  },
+   {
+    id: 3,
+    name: "Bone staight Blunt",
+    price: 125000,
+    image: "https://rainbowhairs.com.ng/cdn/shop/files/146B.jpg?v=1733957871&width=1200"
+  },
+   {
+    id: 3,
+    name: "Long Ponytail Straight Hair",
+    price: 10000,
+    image: "https://img.kwcdn.com/product/fancy/8f1ae3f4-d975-4e3c-a153-311d42d0ecd8.jpg?imageView2/2/w/800/q/70/format/avif"
+  },
+   {
+    id: 3,
+    name: "Straight Human Hair",
+    price: 156000,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSro4LZCl0CwrTyNLeNbXLJ-B1RUmXsRnoxSA&s"
+  },
+   {
+    id: 3,
+    name: "Brazillian Kinky Hair",
+    price: 89000,
+    image: "https://img-1.kwcdn.com/product/fancy/d1d07900-a084-4fbd-a476-77d365f47958.jpg?imageView2/2/w/800/q/70/format/avif"
+  }
+];
+
+export default products;
